acdab9a034858e55ebaa9c63130f0111a5defe42
KhachatryanGor86 <gorpower@bk.ru>
2019-10-20 14:06:45 +0400

    homework1 is added to week1

---
+++ week1/Homework/Problem1.py
@@ -0,0 +1,17 @@
+project = "cake"
+difficulty = 5
+ingredients = ["flour","butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+
+print("apples" in ingredients)
+print("butter" in ingredients)
+print("eggs" in ingredients or "margarine" in ingredients)
+print("eggs" in ingredients and "margarine" in ingredients)
+
+flour, butter, sugar, eggs, cocoa_powder, baking_powder = 175, 175, "100g", 2, "1ts", 0.5
+
+print("flour - ", flour, "g")
+print("butter - ", butter, "g")
+print("sugar - ", sugar)
+print("eggs - ", eggs, "g")
+print("cocoa_powder - ", cocoa_powder)
+print("baking_powder - ", baking_powder, "g")
