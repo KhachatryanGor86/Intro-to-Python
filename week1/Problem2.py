a331e39f1f87c193fc4e59332f1774342e99d6db
KhachatryanGor86 <gorpower@bk.ru>
2019-10-20 12:44:53 +0400

    Practice1 added to week1

---
+++ week1/Problem2.py
@@ -0,0 +1,5 @@
+variables = ["course", "student", "nickname", "grade", "python_expert"]
+values = ["Intro to Python", "Gor Khachatryan", "GorPower", "100", False]
+
+for i in range(0,5):
+    print(variables[i] , ":" , values[i])
