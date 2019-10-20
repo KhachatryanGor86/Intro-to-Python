03bd17bd27e3d3d5263e53b2b1359f7dac00af3e
KhachatryanGor86 <gorpower@bk.ru>
2019-10-20 13:10:30 +0400

    Practice1 updated

--- week1/Problem2.py
+++ week1/Problem2.py
@@ -1,5 +1,5 @@
 variables = ["course", "student", "nickname", "grade", "python_expert"]
 values = ["Intro to Python", "Gor Khachatryan", "GorPower", "100", False]
 
-for i in range(0,5):
+for i in range(len(variables)):
     print(variables[i] , ":" , values[i])
--- week1/Problem4.py
+++ week1/Problem4.py
@@ -1,5 +1,6 @@
+import math
 AB = int(input("Input value for AB side --> "))
 AC = int(input("Input value for AC side --> "))
 
-BC = (AB**2 + AC**2)**(0.5)
+BC = math.sqrt(AB**2 + AC**2)
 print("The hypotenuse of the triangle ABC = ", BC)
